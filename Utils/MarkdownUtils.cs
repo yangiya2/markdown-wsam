03f6d0c606955eeb84e73ff4da00c5081371d1b4
yangiya2 <129474449+yangiya2@users.noreply.github.com>
2024-09-02 20:52:28 +0900

    markdown 表示できるようになったところで登録

--- Utils/MarkdownUtils.cs
+++ Utils/MarkdownUtils.cs
@@ -1,6 +1,8 @@
 ﻿namespace Utils
 {
     using Markdig;
+    using Markdig.Extensions.AutoIdentifiers;
+
     /// <summary>
     /// Markdown に関するユーティリティー
     /// </summary>
@@ -8,7 +10,13 @@
     {
         public static string ToHtml(this string str)
         {
-            var markdownPipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
+            var markdownPipeline = new MarkdownPipelineBuilder()
+                .UsePipeTables()
+                .UseAdvancedExtensions() // 追加の拡張機能を使用
+                .UseAutoIdentifiers(AutoIdentifierOptions.GitHub) // ヘッダーにIDを追加
+                .Build();
+
+            //var markdownPipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
             string htmlContents = Markdown.ToHtml(str, markdownPipeline);
             return htmlContents;
         }
